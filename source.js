ae4bb9b7d2e42ae049c765f69fbf433c5aaf23d5
Angus Fretwell <angusfretwell@me.com>
2016-04-03 22:04:32 +1000

    refactor: use translate and requestionAnimationFrame for superior perfomance

--- source.js
+++ source.js
@@ -2,36 +2,39 @@ import 'core-js/modules/es6.object.assign';
 import 'core-js/modules/es6.array.from';
 
 export function parallax(selector, options) {
-  const windowHeight = window.height;
+  let lastPosition = -1;
 
   // Establish default settings
   const settings = Object.assign({
     speed: 0.15,
   }, options);
 
-  function updateBackgroundPosition(elem) {
-    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    const offset = elem.getBoundingClientRect().top + scrollTop;
-    const height = elem.offsetHeight;
-
-    // // Check if we're above or below the viewport
-    if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-      return;
-    }
-
-    const yBgPosition = Math.round((offset - scrollTop) * settings.speed);
-
-    // Apply the y-axis background position
-    elem.style.backgroundPosition = `center ${yBgPosition * -1}px`;
-  }
-
   const elems = Array.from(document.querySelectorAll(selector));
 
-  // Iterate over each object in collection
-  return elems.forEach((elem) => {
-    // Create an event listener for scrolling
-    document.addEventListener('scroll', () => {
-      updateBackgroundPosition(elem);
-    }, false);
-  });
+  function updatePosition() {
+    elems.forEach((elem) => {
+      const offset = elem.getBoundingClientRect().top + lastPosition;
+      const yPosition = Math.round((offset - lastPosition) * settings.speed);
+
+      // Apply the y-axis transform
+      elem.style.transform = `translate3d(0, ${yPosition * -1}px, 0)`; // eslint-disable-line
+    });
+  }
+
+  function animate() {
+    // If the offset position hasn't changed, skip this frame
+    if (lastPosition === window.pageYOffset) {
+      requestAnimationFrame(animate);
+      return false;
+    }
+
+    // Save the new offset position
+    lastPosition = window.pageYOffset;
+
+    updatePosition();
+    requestAnimationFrame(animate);
+  }
+
+  // Start the animate
+  animate();
 }
